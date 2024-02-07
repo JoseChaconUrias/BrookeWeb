5c231e8d436a72b89bea59c41b25e6b297ea8ebe
Kevin <jcurias123@gmail.com>
2024-02-07 10:10:27 -0600

    fix: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename='/BrookeWeb/'>
+    <HashRouter basename='/BrookeWeb/'>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
