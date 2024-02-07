9750a4944a75af94517dc42b23810290c07b214c
Kevin <jcurias123@gmail.com>
2024-02-06 23:05:49 -0600

    router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import { createBrowserRouter } from 'react-router-dom';
+import { Home } from './_roots/pages';
+import Research from './_roots/pages/Research';
+
+const router = createBrowserRouter([
+    {
+        path:"/BrookeWeb/",
+        element: <App />,
+        children: [
+            {
+                path: "/BrookeWeb/",
+                element: <Home />
+            },
+            {
+                path: "/BrookeWeb/",
+                element: <Research />
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <App />
+        <RouterProvider router={router} />
     </BrowserRouter>
 )
