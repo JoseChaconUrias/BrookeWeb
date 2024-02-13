52df4c01c3e7bd25665ab3e7c6a75d015c0bdbc6
Kevin <jcurias123@gmail.com>
2024-02-13 13:11:18 -0600

    fix: text centered

--- src/_roots/pages/Home.tsx
+++ src/_roots/pages/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <>
       <div className="relative min-h-screen flex items-center justify-center">
-        <div className="absolute text-white text-center top-60 fade-in">
+        <div className="absolute text-white text-center fade-in">
           <h1 className="text-5xl">Hello I'm Brooke McCartney</h1>
         </div>    
       </div>
