14fc5afdfd347010619790b2689b6e1fbeb6843b
Kevin <jcurias123@gmail.com>
2024-02-13 15:21:32 -0600

    fix: mobile background

--- src/_roots/RootLayout.tsx
+++ src/_roots/RootLayout.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useRef } from 'react';
 import { Outlet } from "react-router-dom";
 import MenuBar from "../components/MenuBar";
 
 const RootLayout = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  
+  useEffect(() =>{
+    const video = videoRef.current;
+    if (video){
+      video.play().catch(error => {
+      console.error("Your browser does not support the video tag", error)
+    });
+    }
+
+  },[]);
+
   return (
     <div className="relative h-screen overflow-hidden">
 
       <video
-        src="assets/virusBg.mp4" autoPlay loop muted
+        ref={videoRef}
+        src="assets/virusBg.mp4" autoPlay loop muted playsInline
         className="object-cover w-full h-full fixed top-0 left-0"
       />
 
