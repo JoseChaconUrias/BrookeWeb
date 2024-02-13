0fabfcd0986c487c2962aa978872dc3d91273baa
Kevin <jcurias123@gmail.com>
2024-02-13 14:16:49 -0600

    mod: larger text

--- src/_roots/pages/Contact.tsx
+++ src/_roots/pages/Contact.tsx
@@ -3,8 +3,8 @@ const Contact = () => {
       <>
         <div className="relative min-h-screen flex items-center justify-center">
           <div className="absolute text-white text-center fade-in">
-            <h1 className="text-4xl mt-20">Contact Information</h1>
-            <div className="mt-8">
+            <h1 className="text-5xl mt-20">Contact Information</h1>
+            <div className="mt-8 text-xl">
               <p>Email: <a href="mailto:brookemccartney64@gmail.com">brookemccartney64@gmail.com</a></p>
               <p>LinkedIn: <a href="www.linkedin.com/in/brooke-mccartney-3988a62a0">Brooke McCartney</a> </p>
               <p>Phone: <a href="tel:+4028021809">(402) 802-1809</a> </p>
